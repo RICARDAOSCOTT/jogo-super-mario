f92d03fc075aa7c9804dab10b7e9e3c7f462addf
RICARDAOSCOTT <163008691+RICARDAOSCOTT@users.noreply.github.com>
2024-08-26 10:40:39 -0300

    ..

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ const pipe = document.querySelector('.pipe');
 const jump = () => {
     mario.classList.add('jump');
 
-
-    setTimeout (() =>)
+}
+    setTimeout (() => {
     mario.classList.remove("jump");
 }
+,500 );
 
+
+const loop = setInterval (() => {})
 
 
 
