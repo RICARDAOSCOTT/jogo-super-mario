a77038406496cb3f2a807ccbc617576bf6101d42
RICARDAOSCOTT <163008691+RICARDAOSCOTT@users.noreply.github.com>
2024-08-28 11:18:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,13 @@ const jump = () => {
 
 
 const loop = setInterval (() => {})
+const pipePosition = pipe.offseftleft;
+const marioPosition = +window.getComputedStyle(mario).bottom.replace("px","");
 
+ if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80)
+     pipe.style.animation = "none";
+pipe.style.left
+});
 
 
 
