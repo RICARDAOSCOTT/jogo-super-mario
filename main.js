354d59f5f3b54e5092962ca66833c65e48220293
RICARDAOSCOTT <163008691+RICARDAOSCOTT@users.noreply.github.com>
2024-09-17 08:30:47 -0300

    .

--- main.js
+++ main.js
@@ -30,7 +30,6 @@ const jump = () => {
 }
 ,500 );
 }
-}
 
 const loop = setInterval (() => {})
 const pipePosition = pipe.offseftleft;
@@ -39,7 +38,7 @@ const marioPosition = +window.getComputedStyle(mario).bottom.replace("px","");
  if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80)
      pipe.style.animation = "none";
 pipe.style.left = `${pipePosition}px`;
-});
+);
 
 mario.style.animation = "none";
 mario.style.left = `${pipePosition}px`;
